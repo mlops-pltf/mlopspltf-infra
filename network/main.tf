d0889f8e08f4caea0534a72e4146d4bb717fe868
Saumalya Sarkar <saumalya75@gmail.com>
2025-03-16 23:21:34 +0530

    :construction: MLAIHO-31::CICD::WIP::Build git-action framework for Terraform

--- network/main.tf
+++ network/main.tf
@@ -26,7 +26,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.environment == "tst" ? "20.0.0.0/16" : "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
