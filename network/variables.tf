c0917ae79b422d28401bb3643d2e65488b85ff94
Saumalya Sarkar <saumalya75@gmail.com>
2025-03-16 17:46:02 +0530

    :construction: MLAIHO-31::CICD::Starting to build git-action framework for Terraform

---
+++ network/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  description = "Environemnt Name"
+  type        = string
+}
+
+variable "allowed_account_id" {
+  description = "Allowed account ID"
+  type        = string
+}
