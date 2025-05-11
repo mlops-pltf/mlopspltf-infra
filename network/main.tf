e7abbd8a39481754de39bb34764932746609da3f
Saumalya Sarkar <saumalya75@gmail.com>
2025-05-11 19:35:36 +0530

    :construction: MLAIHO-57::CICD::Adding required network setup for ollama host node

--- network/main.tf
+++ network/main.tf
@@ -89,3 +89,47 @@ resource "aws_route_table_association" "mlopspltf_public_subnet_1_rt_associaton"
   subnet_id      = aws_subnet.mlopspltf_public_subnet_1.id
   route_table_id = aws_route_table.mlopspltf_route_table_with_igw.id
 }
+
+
+resource "aws_security_group" "mloppltf_ollama_node_security_group" {
+  name        = "mloppltf_${var.environment}_ollama_node_sg"
+  description = "Allow required traffic for ollma inference node"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name        = "mloppltf_${var.environment}_ollama_node_sg"
+    project     = "MLOPSPLTF"
+    environment = "${var.environment}"
+    jira-ticket = "MLAIHO-57"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "backdoor_to_my_mac" {
+  security_group_id = aws_security_group.mloppltf_ollama_node_security_group.id
+  cidr_ipv4         = "106.212.85.7/32"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_originated_within_vpc" {
+  security_group_id = aws_security_group.mloppltf_ollama_node_security_group.id
+  cidr_ipv6         = aws_vpc.main.cidr_block
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_originated_within_vpc" {
+  security_group_id = aws_security_group.mloppltf_ollama_node_security_group.id
+  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.mloppltf_ollama_node_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.mloppltf_ollama_node_security_group.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
