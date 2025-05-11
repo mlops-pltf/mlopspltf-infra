0e7ccdeab3aa3fca2b8d17b9e19dcb8801aa9e67
Saumalya Sarkar <saumalya75@gmail.com>
2025-05-11 20:03:37 +0530

    :construction: MLAIHO-57::CICD::Adding required network setup for ollama host node

--- network/main.tf
+++ network/main.tf
@@ -54,11 +54,6 @@ resource "aws_route_table" "mlopspltf_route_table_with_igw" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = aws_vpc.main.cidr_block
-    gateway_id = "local"
-  }
-
-  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.mlopspltf_internet_gateway.id
   }
